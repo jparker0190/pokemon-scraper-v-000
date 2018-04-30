835efcdd26381dacde6488d1f00fad28bc0ea7ab
Joseph Parker <jparker010190@gmail.com>
2018-04-30 23:14:58 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,21 +1,11 @@
 class Pokemon
   attr_accessor :name, :type, :db
 
-  def initialize(id=nil, name, type, db)
+  def initialize(id:, name, type, db)
     @name = name
     @type = type
     @db = db
     @id = id
   end
-  def save
-    sql = <<-SQL 
-    INSERT INTO pokemon (name, type) VALUES (?,?)
-    SQL
-    
-  end
-  def self.create(name:, type:)
-    song = Song.new(name, type)
-    song.save
-    song
-  end
+
 end
