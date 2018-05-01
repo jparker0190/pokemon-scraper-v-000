be1ec60d28a1e7681d5a17c39c350715ea00670b
Joseph Parker <jparker010190@gmail.com>
2018-05-01 00:12:46 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -9,6 +9,11 @@ class Pokemon
   def self.save(name, type, db)
     db.execute("INSERT INTO pokemon (name, type) VALUES (?,?)", name, type)
   end
+  def self.new_from_db(row)
+    new_polk = self.new
+    new_polk.id = row[0]
+    new_polk.name = row[1]
+    new_polk.type = row[2]
   def self.find(id, db)
     db.execute("SELECT * FROM pokemon where id = ? LIMIT 1", id)
   end
